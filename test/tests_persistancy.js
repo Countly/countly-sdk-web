32d55e547fa414f32d5395f21931afbcd4a468be
Furkan Başaran <frknbasaran@gmail.com>
2018-09-20 18:11:32 +0300

    [web-sdk] persistancy test changes.
    - events and other things removed from html.
    - add_event and other methods which created data on storage moved into first load of casperjs.
    - several test_mode controls added into countly web sdk, for hold eventQueue and requestQueue values.

--- test/tests_persistancy.js
+++ test/tests_persistancy.js
@@ -5,51 +5,61 @@ function exists(value){
 function notEmpty(value){
     return (value.length > 0) ? true : false;
 }
-casper.test.begin("Testing example_persistancy.html", 20, function(test) {
+
+/*
+* Web SDK Persistancy Test
+* Purpose: Be sure store() method works correctly and related variables has perceives right values from storage. 
+* Method: Load page. Define variables into storage, then reload and check variables and storage values.
+*/
+casper.test.begin("Testing example_persistancy.html", 14, function(test) {
     var tests = [];
     
-    /*
-    * Print console logs from casper
-    */
+    // Print console logs from casper
     casper.on('remote.message', function(message) {
         this.echo(message);
     });
 
     casper.start(fs.workingDirectory + "/examples/example_persistancy.html", function() {
         
+        // load page first time
         casper.evaluate(function() {
-            // define temporary values into localStorage
-            Countly._internals.store('temp_cly_event', Countly._internals.store('cly_event'));
-            Countly._internals.store('temp_cly_queue', Countly._internals.store('cly_queue'));
-            Countly._internals.store('temp_cly_id', Countly._internals.store('cly_id'));
-            Countly._internals.store('temp_cly_token', Countly._internals.store('cly_token'));
-            Countly._internals.store('temp_cly_old_token', Countly._internals.store('cly_old_token'));
-            Countly._internals.store('temp_cly_ignore', Countly._internals.store('cly_ignore'));
-            Countly._internals.store('temp_cly_cmp_id', Countly._internals.store('cly_cmp_id'));
-            Countly._internals.store('temp_cly_cmp_uid', Countly._internals.store('cly_cmp_uid'));
+            // for test: cly_event
+            Countly.add_event({'key':'homepage'});
+
+            // for test: cly_ignore
+            Countly.opt_out();
+            Countly.opt_in();
+            
+            // for test: cly_queue
+            Countly.begin_session();
+            Countly.session_duration(100);
+            Countly.end_session();
+            Countly.remove_consent('views');
+            // Move into temporary places from stored cmp_id and cmp_uid values.
+            // call set token method for define cly_token value in storage.
+            Countly._internals.setToken('TOKEN_STRING');
         });
 
+        // reload page
         this.reload(function() {
+
             // get stored and current values of sdk variables
             var values = this.evaluate(function() {
+
                 var stored = {};
                 var current = {};
                 
-                stored.cly_id = Countly._internals.store('temp_cly_id');
-                stored.cly_queue = Countly._internals.store('temp_cly_queue');
-                stored.cly_event = Countly._internals.store('temp_cly_event');
-                stored.cly_token = Countly._internals.store('temp_cly_token');
-                stored.cly_old_token = Countly._internals.store('temp_cly_old_token');
-                stored.cly_ignore = Countly._internals.store('temp_cly_ignore');
-                stored.cly_cmp_id = Countly._internals.store('temp_cly_cmp_id');
-                stored.cly_cmp_uid = Countly._internals.store('temp_cly_cmp_uid');
-
+                // expose values 
+                stored.cly_id = Countly._internals.store('cly_id');
                 current.cly_id = Countly.device_id;
-                current.cly_queue = Countly._internals.store('cly_queue');
-                current.cly_event = Countly._internals.store('cly_event');
-                current.cly_token = Countly._internals.store('cly_token');
-                current.cly_old_token = Countly._internals.store('cly_old_token');
-                current.cly_ignore = Countly._internals.store('cly_ignore');
+                stored.cly_queue = Countly._internals.store('cly_queue');
+                current.cly_queue = Countly._internals.getRequestQueue();
+                stored.cly_event = Countly._internals.store('cly_event');
+                current.cly_event = Countly._internals.getEventQueue();
+                stored.cly_token = Countly._internals.store('cly_token');
+                current.cly_token = Countly._internals.getToken();
+                stored.cly_ignore = Countly._internals.store('cly_ignore');
+                current.cly_ignore = Countly.ignore_visitor;
                 current.cly_cmp_id = Countly._internals.store('cly_cmp_id');
                 current.cly_cmp_uid = Countly._internals.store('cly_cmp_uid');
                 
@@ -58,34 +68,27 @@ casper.test.begin("Testing example_persistancy.html", 20, function(test) {
                     current: current
                 };
             });
-            
-            // check for is empty non-json (string) values
+
+            // these values should not be empty
             test.assert(notEmpty(values.current.cly_id));
             test.assert(notEmpty(values.stored.cly_id));
             test.assert(notEmpty(values.current.cly_token));
             test.assert(notEmpty(values.stored.cly_token));
-            test.assert(notEmpty(values.current.cly_old_token));
-            test.assert(notEmpty(values.stored.cly_old_token));
             test.assert(notEmpty(values.current.cly_cmp_uid));
-            test.assert(notEmpty(values.stored.cly_cmp_uid));
             test.assert(notEmpty(values.current.cly_cmp_id));
-            test.assert(notEmpty(values.stored.cly_cmp_id));
+            
 
             // check for exist non-string values
             test.assert(exists(values.current.cly_ignore));
             test.assert(exists(values.stored.cly_ignore));
             // check for equality
             test.assertEquals(values.current.cly_id, values.stored.cly_id);
-            test.assertEquals(values.current.cly_token, values.stored.cly_token);
-            test.assertEquals(values.current.cly_old_token, values.stored.cly_old_token);
             test.assertEquals(values.current.cly_ignore, values.stored.cly_ignore);
-            test.assertEquals(values.current.cly_cmp_uid, values.stored.cly_cmp_uid);
-            test.assertEquals(values.current.cly_cmp_id, values.stored.cly_cmp_id);
-            
-            // should be added one more event too cly_event array 
-            // and should be added 5 more request too cly_queue array
-            test.assertEquals(values.current.cly_event.length, values.stored.cly_event.length + 1);
-            test.assertEquals(values.current.cly_queue.length, values.stored.cly_queue.length + 5);
+            test.assertEquals(values.current.cly_cmp_uid, 'test_campaign_uid');
+            test.assertEquals(values.current.cly_cmp_id, 'test_campaign_id');
+
+            test.assertEquals(JSON.stringify(values.current.cly_event), JSON.stringify(values.stored.cly_event));
+            test.assertEquals(JSON.stringify(values.current.cly_queue), JSON.stringify(values.stored.cly_queue));
         });
     })
     .run(function() {
