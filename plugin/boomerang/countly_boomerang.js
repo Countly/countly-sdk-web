33c2e21f1412161a5de549c1b1fa41719490ac9a
Arturs Sosins <ar2rsawseen@gmail.com>
2020-10-17 19:38:08 +0300

    Create separate Countly class

--- plugin/boomerang/countly_boomerang.js
+++ plugin/boomerang/countly_boomerang.js
@@ -1,119 +1,122 @@
 "use strict";
-/*global Countly */
+/*global Countly, CountlyClass */
 /*
 Countly APM based on Boomerang JS
 */
-(function () {
+(function cly_load_track_performance() {
     Countly = Countly || {}; // eslint-disable-line no-global-assign
     Countly.onload = Countly.onload || [];
+    if (typeof CountlyClass === "undefined") {
+        return Countly.onload.push(cly_load_track_performance);
+    }
     /**
      *  Enables tracking performance through boomerang.js
      *  @memberof Countly
      *  @param {object} config - Boomerang js configuration
      */
-    Countly.track_performance = function (config) {
-        Countly.onload.push(function () {
-            config = config || {};
-            var initedBoomr = false;
-            function initBoomerang(BOOMR){
-                if (BOOMR && !initedBoomr) {
-                    BOOMR.subscribe("before_beacon", function(beaconData) {
-                        Countly._internals.log("BOOMR", "before_beacon", JSON.stringify(beaconData, null, 2));
-                        var trace = {};
-                        if (beaconData["rt.start"] !== "manual" && !beaconData["http.initiator"] && beaconData["rt.quit"] !== ""){
-                            trace.type = "device";
-                            trace.apm_metrics = {};
-                            if (typeof beaconData["pt.fp"] !== "undefined") {
-                                trace.apm_metrics.first_paint = beaconData["pt.fp"];
-                            }
-                            else if (typeof beaconData["nt_first_paint"] !== "undefined") {
-                                trace.apm_metrics.first_paint = beaconData["nt_first_paint"] - beaconData["rt.tstart"];
-                            }
-                            if (typeof beaconData["pt.fcp"] !== "undefined") {
-                                trace.apm_metrics.first_contentful_paint = beaconData["pt.fcp"];
-                            }
-                            if (typeof beaconData["nt_domint"] !== "undefined") {
-                                trace.apm_metrics.dom_interactive = beaconData["nt_domint"] - beaconData["rt.tstart"];
-                            }
-                            if (typeof beaconData["nt_domcontloaded_st"] !== "undefined" && typeof beaconData["nt_domcontloaded_end"] !== "undefined") {
-                                trace.apm_metrics.dom_content_loaded_event_end = beaconData["nt_domcontloaded_end"] - beaconData["nt_domcontloaded_st"];
-                            }
-                            if (typeof beaconData["nt_load_st"] !== "undefined" && typeof beaconData["nt_load_end"] !== "undefined") {
-                                trace.apm_metrics.load_event_end = beaconData["nt_load_end"] - beaconData["nt_load_st"];
-                            }
-                            if (typeof beaconData["c.fid"] !== "undefined") {
-                                trace.apm_metrics.first_input_delay = beaconData["c.fid"];
-                            }
+
+    CountlyClass.prototype.track_performance = function (config) {
+        var self = this;
+        config = config || {};
+        var initedBoomr = false;
+        function initBoomerang(BOOMR){
+            if (BOOMR && !initedBoomr) {
+                BOOMR.subscribe("before_beacon", function(beaconData) {
+                    self._internals.log("BOOMR", "before_beacon", JSON.stringify(beaconData, null, 2));
+                    var trace = {};
+                    if (beaconData["rt.start"] !== "manual" && !beaconData["http.initiator"] && beaconData["rt.quit"] !== ""){
+                        trace.type = "device";
+                        trace.apm_metrics = {};
+                        if (typeof beaconData["pt.fp"] !== "undefined") {
+                            trace.apm_metrics.first_paint = beaconData["pt.fp"];
                         }
-                        else if (beaconData["http.initiator"] && ["xhr", "spa", "spa_hard"].indexOf(beaconData["http.initiator"]) !== -1) {
-                            var headers = beaconData["http.hdr"] || {};
-                            trace.type = "network";
-                            trace.apm_metrics = {
-                                response_time: beaconData.t_done,
-                                response_payload_size: beaconData["nt_dec_size"],
-                                request_payload_size: headers["Content-Size"],
-                                response_code: (typeof beaconData["http.errno"] !== "undefined") ? beaconData["http.errno"] : 200
-                            };
+                        else if (typeof beaconData["nt_first_paint"] !== "undefined") {
+                            trace.apm_metrics.first_paint = beaconData["nt_first_paint"] - beaconData["rt.tstart"];
                         }
-                        else if (beaconData["http.initiator"] === "interaction") {
-                            trace.apm_metrics = {};
-                            var hasData = false;
-                            if (typeof beaconData["c.f.l"] !== "undefined") {
-                                trace.apm_metrics.slow_rendering_frames = beaconData["c.f.l"];
-                                hasData = true;
-                            }
-                            if (typeof beaconData["c.b"] !== "undefined") {
-                                trace.apm_metrics.frozen_frames = beaconData["c.b"];
-                                hasData = true;
-                            }
-                            if (hasData) {
-                                trace.type = "device";
-                            }
+                        if (typeof beaconData["pt.fcp"] !== "undefined") {
+                            trace.apm_metrics.first_contentful_paint = beaconData["pt.fcp"];
                         }
-                        if (trace.type) {
-                            trace.name = (beaconData.u + "").split("//").pop().split("?")[0];
-                            trace.stz = beaconData["rt.tstart"];
-                            trace.etz = beaconData["rt.end"];
-                            Countly.report_trace(trace);
+                        if (typeof beaconData["nt_domint"] !== "undefined") {
+                            trace.apm_metrics.dom_interactive = beaconData["nt_domint"] - beaconData["rt.tstart"];
                         }
-                    });
-                    BOOMR.xhr_excludes = BOOMR.xhr_excludes || {};
-                    BOOMR.xhr_excludes[Countly.url.split("//").pop()] = true;
-                    //config.beacon_url = config.beacon_url || Countly.url;
-                    if (typeof config.beacon_disable_sendbeacon === "undefined") {
-                        config.beacon_disable_sendbeacon = true;
+                        if (typeof beaconData["nt_domcontloaded_st"] !== "undefined" && typeof beaconData["nt_domcontloaded_end"] !== "undefined") {
+                            trace.apm_metrics.dom_content_loaded_event_end = beaconData["nt_domcontloaded_end"] - beaconData["nt_domcontloaded_st"];
+                        }
+                        if (typeof beaconData["nt_load_st"] !== "undefined" && typeof beaconData["nt_load_end"] !== "undefined") {
+                            trace.apm_metrics.load_event_end = beaconData["nt_load_end"] - beaconData["nt_load_st"];
+                        }
+                        if (typeof beaconData["c.fid"] !== "undefined") {
+                            trace.apm_metrics.first_input_delay = beaconData["c.fid"];
+                        }
                     }
-                    BOOMR.init(config);
-                    BOOMR.t_end = new Date().getTime();
-                    Countly.BOOMR = BOOMR;
-                    initedBoomr = true;
-                    Countly._internals.log("Boomerang initiated:", config);
+                    else if (beaconData["http.initiator"] && ["xhr", "spa", "spa_hard"].indexOf(beaconData["http.initiator"]) !== -1) {
+                        var headers = beaconData["http.hdr"] || {};
+                        trace.type = "network";
+                        trace.apm_metrics = {
+                            response_time: beaconData.t_done,
+                            response_payload_size: beaconData["nt_dec_size"],
+                            request_payload_size: headers["Content-Size"],
+                            response_code: (typeof beaconData["http.errno"] !== "undefined") ? beaconData["http.errno"] : 200
+                        };
+                    }
+                    else if (beaconData["http.initiator"] === "interaction") {
+                        trace.apm_metrics = {};
+                        var hasData = false;
+                        if (typeof beaconData["c.f.l"] !== "undefined") {
+                            trace.apm_metrics.slow_rendering_frames = beaconData["c.f.l"];
+                            hasData = true;
+                        }
+                        if (typeof beaconData["c.b"] !== "undefined") {
+                            trace.apm_metrics.frozen_frames = beaconData["c.b"];
+                            hasData = true;
+                        }
+                        if (hasData) {
+                            trace.type = "device";
+                        }
+                    }
+                    if (trace.type) {
+                        trace.name = (beaconData.u + "").split("//").pop().split("?")[0];
+                        trace.stz = beaconData["rt.tstart"];
+                        trace.etz = beaconData["rt.end"];
+                        self.report_trace(trace);
+                    }
+                });
+                BOOMR.xhr_excludes = BOOMR.xhr_excludes || {};
+                BOOMR.xhr_excludes[self.url.split("//").pop()] = true;
+                //config.beacon_url = config.beacon_url || Countly.url;
+                if (typeof config.beacon_disable_sendbeacon === "undefined") {
+                    config.beacon_disable_sendbeacon = true;
                 }
-                else {
-                    Countly._internals.log("Boomerang called without its instance or was already initialized");
-                }
-            }
-            if (window.BOOMR) {
-                initBoomerang(window.BOOMR);
+                BOOMR.init(config);
+                BOOMR.t_end = new Date().getTime();
+                self.BOOMR = BOOMR;
+                initedBoomr = true;
+                self._internals.log("Boomerang initiated:", config);
             }
             else {
-                Countly._internals.log("Boomerang not yet loaded, waiting for it to load");
-                // Modern browsers
-                if (document.addEventListener) {
-                    document.addEventListener("onBoomerangLoaded", function(e) {
-                        initBoomerang(e.detail.BOOMR);
-                    });
-                }
-                // IE 6, 7, 8 we use onPropertyChange and look for propertyName === "onBoomerangLoaded"
-                else if (document.attachEvent) {
-                    document.attachEvent("onpropertychange", function(e) {
-                        if (!e) {e = event;}
-                        if (e.propertyName === "onBoomerangLoaded") {
-                            initBoomerang(e.detail.BOOMR);
-                        }
-                    });
-                }
+                self._internals.log("Boomerang called without its instance or was already initialized");
             }
-        });
+        }
+        if (window.BOOMR) {
+            initBoomerang(window.BOOMR);
+        }
+        else {
+            self._internals.log("Boomerang not yet loaded, waiting for it to load");
+            // Modern browsers
+            if (document.addEventListener) {
+                document.addEventListener("onBoomerangLoaded", function(e) {
+                    initBoomerang(e.detail.BOOMR);
+                });
+            }
+            // IE 6, 7, 8 we use onPropertyChange and look for propertyName === "onBoomerangLoaded"
+            else if (document.attachEvent) {
+                document.attachEvent("onpropertychange", function(e) {
+                    if (!e) {e = event;}
+                    if (e.propertyName === "onBoomerangLoaded") {
+                        initBoomerang(e.detail.BOOMR);
+                    }
+                });
+            }
+        }
     };
 }());
