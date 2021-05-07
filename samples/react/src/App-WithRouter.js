975b1f338c379aa848fa65df7698724515392cbf
prikshittekta <prikshittekta@gmail.com>
2021-05-07 17:32:31 +0530

    crashes for react apps

--- samples/react/src/App-WithRouter.js
+++ samples/react/src/App-WithRouter.js
@@ -1,99 +1,46 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from './Components/Home';
-import Contact from './Components/Contact';
-import Header from './Components/Header';
-import Countly from 'countly-sdk-web';
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+import Header from "./Components/Header";
 
-import Location from './Location-WithRouter';
+import Location from "./Location-WithRouter";
+import ErrorBoundary from "./ErrorBoundary";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loginState: localStorage.getItem("clydemo-login-state") ? true: false
-    }
-  }
-
-  setLoginState(state) {
-    this.setState({
-      loginState: state
-    })
-  }
-
-  componentDidMount() {
-    //Exposing Countly to the DOM as a global variable
-    //Usecase - Heatmaps
-    window.Countly = Countly;
-    Countly.init({
-      app_key: 'YOUR_APP_KEY',
-      url: 'YOUR_SERVER_URL',
-      session_update: 10,
-      use_session_cookie: false,
-      debug: false,
-      require_consent: true,
-      namespace: "react-demo",
-      inactivity_time: 1,
-      offline_mode: false,
-      // device_id: "cly-device-demo-id", //Set only if you want dont want to use countly generated device_id
-    });
-
-    //Since Countly is loaded and available, you can use synchronus or asynchronus calls, does not matter
-    Countly.q.push(['group_features', {
-        activity: ["sessions", "events", "views", "location"],
-        interaction: ["scrolls", "clicks"],
-        whereabouts: ["users"]
-    }]);
-
-    if (typeof(localStorage) !== "undefined") {
-        var consents = localStorage.getItem("consents");
-
-        if(consents){
-            Countly.q.push(['add_consent', JSON.parse(consents)]);
-        }
-        else{
-            var consent = window.confirm("We are going to track you. Do you give your consent ?");
-            consents = ["activity", "interaction", "whereabouts"];
-            if(consent) {
-                Countly.q.push(['add_consent', consents]);
-                localStorage.setItem("consents", JSON.stringify(consents));
-            }
-            else {
-                Countly.q.push(['remove_consent', consents]);
-                localStorage.removeItem("consents");
-            }
-        }
+    constructor(props) {
+        super(props);
+        this.state = {
+            loginState: localStorage.getItem("clydemo-login-state") ? true : false
+        };
     }
 
-    Countly.q.push(['track_sessions']);
-    Countly.q.push(['track_pageview']);
-    Countly.q.push(['track_scrolls']);
-    Countly.q.push(['track_clicks']);
-    Countly.q.push(['track_links']);
-  }
+    setLoginState(state) {
+        this.setState({
+            loginState: state
+        });
+    }
 
-  render () {
-    return (
-      <Router basename={`${process.env.PUBLIC_URL}`}>
-        <Location>
-          <Header loginState={this.state.loginState} setLoginState={(state) => this.setLoginState(state)}/>
-          <Switch>
-            <Route path="/contact">
-              <Contact />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </Location>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router basename={`${process.env.PUBLIC_URL}`}>
+                <ErrorBoundary>
+                    <Location>
+                        <Header loginState={this.state.loginState} setLoginState={(state) => this.setLoginState(state)} />
+                        <Switch>
+                            <Route path="/contact">
+                                <Contact />
+                            </Route>
+                            <Route path="/">
+                                <Home />
+                            </Route>
+                        </Switch>
+                    </Location>
+                </ErrorBoundary>
+            </Router>
+        );
+    }
 }
 
 export default App;
