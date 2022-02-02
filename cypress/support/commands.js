09eff674debf45e9a7de58346ca2842f09812021
turtledreams <deniztaygunerten@gmail.com>
2022-02-02 17:34:59 +0300

    improved the logic and added event check

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,20 +23,57 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.add('check_begin_session', (clyQueue, appKey) => {
-    let queue = JSON.parse(clyQueue)[0];
-    let metrics = JSON.parse(queue.metrics);
-    expect(queue.begin_session).to.equal(1);
-    expect(metrics._app_version).to.be.ok;
-    expect(metrics._ua).to.be.ok;
-    expect(metrics._resolution).to.be.ok;
-    expect(metrics._density).to.be.ok;
-    expect(metrics._locale).to.be.ok;
-    expect(queue.app_key).to.equal(appKey);
-    expect(queue.device_id).to.equal("[CLY]_temp_id");
-    expect(queue.sdk_name).to.equal("javascript_native_web");
-    expect(queue.sdk_version).to.be.ok;
+Cypress.Commands.add('check_commons', (queue) => {
     expect(queue.timestamp).to.be.ok;
     expect(queue.hour).to.be.within(0, 23);
     expect(queue.dow).to.be.within(1, 7);
+})
+
+Cypress.Commands.add('check_begin_session', () => {
+    cy.fixture('variables').then((ob) => {
+        cy.getLocalStorage(`${ob.appKey}/cly_queue`).then(
+            (e) => {
+                let queue = JSON.parse(e)[0];
+                let metrics = JSON.parse(queue.metrics);
+                expect(queue.begin_session).to.equal(1);
+                expect(metrics._app_version).to.be.ok;
+                expect(metrics._ua).to.be.ok;
+                expect(metrics._resolution).to.be.ok;
+                expect(metrics._density).to.be.ok;
+                expect(metrics._locale).to.be.ok;
+                expect(queue.app_key).to.equal(ob.appKey);
+                expect(queue.device_id).to.equal("[CLY]_temp_id");
+                expect(queue.sdk_name).to.equal("javascript_native_web");
+                expect(queue.sdk_version).to.be.ok;
+                cy.check_commons(queue);
+            }
+        );
+    });
+})
+Cypress.Commands.add('check_event', (eventObject) => {
+    cy.fixture('variables').then((ob) => {
+        cy.getLocalStorage(`${ob.appKey}/cly_event`).then(
+            (e) => {
+                let queue = JSON.parse(e)[0];
+                expect(queue.key).to.equal(eventObject.key);
+                if (typeof eventObject.count === 'undefined') {
+                    expect(queue.count).to.equal(1);
+                } else {
+                    expect(queue.count).to.equal(eventObject.count);
+                }
+                if (typeof eventObject.sum !== 'undefined') {
+                    expect(queue.sum).to.equal(eventObject.sum);
+                }
+                if (typeof eventObject.dur !== 'undefined') {
+                    expect(queue.dur).to.equal(eventObject.dur);
+                }
+                if (typeof eventObject.segmentation !== 'undefined') {
+                    for (var key in eventObject.segmentation) {
+                    expect(queue.segmentation[key]).to.equal(eventObject.segmentation[key]);
+                    }
+                }
+                cy.check_commons(queue);
+            }
+        );
+    });
 })
