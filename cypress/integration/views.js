dc95dc1eca304508c08cad079882be56a4819dce
turtledreams <deniztaygunerten@gmail.com>
2022-02-24 15:52:10 +0300

    .

--- cypress/integration/views.js
+++ cypress/integration/views.js
@@ -45,13 +45,13 @@ describe('Views tests ', () => {
         hp.haltAndClearStorage(() => {
             initMain();
             Countly.track_view(pageNameOne);
-            cy.wait(5500).then(() => {
+            cy.wait(6500).then(() => {
                 Countly.track_view(pageNameTwo);
                 cy.fetch_local_event_queue().then((eq) => {
                     expect(eq.length).to.equal(3);
                     cy.check_view_event(eq[0], pageNameOne);
                     // this test is flaky we are expecting 3 and +1 (4) to make test more reliable 
-                    cy.check_view_event(eq[1], pageNameOne, 5);
+                    cy.check_view_event(eq[1], pageNameOne, 6);
                     cy.check_view_event(eq[2], pageNameTwo);
                 });
             });
