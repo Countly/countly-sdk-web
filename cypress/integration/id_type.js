7964674fefeba3782237a5adc6269eb2a84a3d00
turtledreams <deniztaygunerten@gmail.com>
2022-02-25 18:53:59 +0300

    id_type

---
+++ cypress/integration/id_type.js
@@ -0,0 +1,14 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+/* eslint-disable require-jsdoc */
+var Countly = require("../../lib/countly");
+var hp = require("../support/helper");
+
+function initMain() {
+    Countly.init({
+        app_key: "YOUR_APP_KEY",
+        url: "https://try.count.ly",
+        session_update: 3,
+        tests: true,
+        max_events: -1
+    });
+}
