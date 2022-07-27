d16b5cc416c4de1c62ee8681c1f58d14a911f1ef
turtledreams <62231246+turtledreams@users.noreply.github.com>
2022-07-27 11:09:59 +0300

    [Web] Mpa 2.0 and other example fixes (#239)
    * formatted index
    
    * some formatting and ordering
    
    * header color change and function separation
    
    * banner color change and logo
    
    * highlight button
    
    * new comments and button change, dynamic media
    
    * wallpaper size
    
    * background color
    
    * apm example correction
    
    * seperated some random functions and dummy data
    
    * empty space
    
    * 2 space
    
    * name and inline styling changes
    
    * final changes
    
    * got rid of hello
    
    * gap
    
    * example fixes
    
    * seperated style
    
    * notyet
    
    * readme and all examples have banner and title
    
    * lf
    
    * lf2
    
    * codacy format
    
    * dummy
    
    * codacycss
    
    * codcy
    
    * angular example
    
    * codacy ang
    
    * codacy changes
    
    * karma fix
    
    * anchor and karma
    
    * buitified helper func
    
    * some linting errors we hidden
    
    * codacy

---
+++ examples/mpa/dummy_users.js
@@ -0,0 +1,86 @@
+"use strict";
+
+// dummy user info
+// eslint-disable-next-line no-unused-vars
+var users = [{
+    device_id: "dummy-1",
+    name: "User-1",
+    username: "User-1",
+    email: "User-1@cly.com",
+    organization: "Countly",
+    phone: "+919882201994",
+    gender: "M",
+    byear: "1994"
+}, {
+    device_id: "dummy-2",
+    name: "User-2",
+    username: "User-2",
+    email: "User-2@cly.com",
+    organization: "Countly",
+    phone: "+919882201994",
+    gender: "M",
+    byear: "1994"
+}, {
+    device_id: "dummy-3",
+    name: "User-3",
+    username: "User-3",
+    email: "User-3@cly.com",
+    organization: "Countly",
+    phone: "+919882201994",
+    gender: "M",
+    byear: "1994"
+}, {
+    device_id: "dummy-4",
+    name: "User-4",
+    username: "User-4",
+    email: "User-4@cly.com",
+    organization: "Countly",
+    phone: "+919882201994",
+    gender: "M",
+    byear: "1994"
+}, {
+    device_id: "dummy-5",
+    name: "User-5",
+    username: "User-5",
+    email: "User-5@cly.com",
+    organization: "Countly",
+    phone: "+919882201994",
+    gender: "M",
+    byear: "1994"
+}, {
+    device_id: "dummy-6",
+    name: "User-6",
+    username: "User-6",
+    email: "User-6@cly.com",
+    organization: "Countly",
+    phone: "+919882201994",
+    gender: "M",
+    byear: "1994"
+}, {
+    device_id: "dummy-7",
+    name: "User-7",
+    username: "User-7",
+    email: "User-7@cly.com",
+    organization: "Countly",
+    phone: "+919882201994",
+    gender: "M",
+    byear: "1994"
+}, {
+    device_id: "dummy-8",
+    name: "User-8",
+    username: "User-8",
+    email: "User-8@cly.com",
+    organization: "Countly",
+    phone: "+919882201994",
+    gender: "M",
+    byear: "1994"
+}, {
+    device_id: "dummy-9",
+    name: "User-9",
+    username: "User-9",
+    email: "User-9@cly.com",
+    organization: "Countly",
+    phone: "+919882201994",
+    gender: "M",
+    byear: "1994"
+}];
