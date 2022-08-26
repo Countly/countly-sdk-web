40f5c81c24c97a53e05b8765ba092e4d01988d65
turtledreams <deniztaygunerten@gmail.com>
2022-08-26 17:04:23 +0300

    more tests

--- cypress/integration/storage.js
+++ cypress/integration/storage.js
@@ -2,166 +2,214 @@
 var Countly = require("../../lib/countly");
 var hp = require("../support/helper");
 
-function initMain() {
+function initMain(val) {
     Countly.init({
         app_key: "YOUR_APP_KEY",
         url: "https://try.count.ly",
         max_events: -1,
         test_mode: true,
-        debug: true
+        debug: true,
+        storage: val
     });
 }
+
 const valueToStore = "value";
 const key = "key";
+const testArray = ["default", "cookie", "none", "localstorage", "randomValue"];
 
-// check basic functionality for local storage. No rawKey or cookies.
-describe("Storage tests, local storage ", () => {
-    it("Checks if setValueInStorage function stores a value correctly", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            Countly._internals.setValueInStorage(key, valueToStore);
-            cy.getLocalStorage(`${hp.appKey}/${key}`).then((value) => {
-                expect(value).to.equal(valueToStore);
-            });
-        });
-    });
-    it("Checks if getValueFromStorage function gets a value correctly", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            Countly._internals.setValueInStorage(key, valueToStore);
-            expect(valueToStore).to.equal(Countly._internals.getValueFromStorage(key));
-        });
-    });
-    it("Checks if getValueFromStorage function can not get a value if it does not exist", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            expect(null).to.equal(Countly._internals.getValueFromStorage(key));
-        });
-    });
-    it("Checks if removeValueFromStorage function removes a value correctly", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            Countly._internals.setValueInStorage(key, valueToStore);
-            expect(valueToStore).to.equal(Countly._internals.getValueFromStorage(key));
-            Countly._internals.removeValueFromStorage(key);
-            expect(null).to.equal(Countly._internals.getValueFromStorage(key));
-        });
-    });
-});
+for (let i = 0; i < 5; i++) {
+    const flag = testArray[i];
+    const isCookie = flag === "cookie";
+    const isLocal = flag === "localstorage";
+    const isNone = flag === "none";
 
-// check basic functionality for cookies. No rawKey or localstorage.
-describe("Storage tests, cookies ", () => {
-    it("Checks if setValueInStorage function stores a cookie correctly", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            Countly._internals.setValueInStorage(key, valueToStore, false);
-            cy.getLocalStorage(`${hp.appKey}/${key}`).then((value) => {
-                expect(value).to.equal(null);
+    describe("Storage tests, storage: " + flag, () => {
+        // for everything at default
+        describe("basic setting", () => {
+            it("Checks if setValueInStorage function stores a value correctly", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    Countly._internals.setValueInStorage(key, valueToStore);
+                    cy.getLocalStorage(`${hp.appKey}/${key}`).then((value) => {
+                        if (isCookie) {
+                            expect(value).to.equal(null);
+                            expect(document.cookie).to.include(`${hp.appKey}/${key}=${valueToStore}`);
+                        }
+                        else if (isNone) {
+                            expect(value).to.equal(null);
+                            expect(document.cookie).to.equal("");
+                        }
+                        else {
+                            expect(value).to.equal(valueToStore);
+                        }
+                    });
+                });
             });
-            expect(document.cookie).to.equal(`${hp.appKey}/${key}=${valueToStore}`);
+            it("Checks if getValueFromStorage function gets a value correctly", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    if (isNone) {
+                        expect(document.cookie).to.equal("");
+                    }
+                    Countly._internals.setValueInStorage(key, valueToStore);
+                    expect(isNone ? undefined : valueToStore).to.equal(Countly._internals.getValueFromStorage(key));
+                });
+            });
+            it("Checks if getValueFromStorage function can not get a value if it does not exist", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    expect(isNone ? undefined : null).to.equal(Countly._internals.getValueFromStorage(key));
+                });
+            });
+            it("Checks if removeValueFromStorage function removes a value correctly", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    if (isNone) {
+                        expect(document.cookie).to.equal("");
+                    }
+                    Countly._internals.setValueInStorage(key, valueToStore);
+                    expect(isNone ? undefined : valueToStore).to.equal(Countly._internals.getValueFromStorage(key));
+                    Countly._internals.removeValueFromStorage(key);
+                    expect(isNone ? undefined : null).to.equal(Countly._internals.getValueFromStorage(key));
+                });
+            });
         });
-    });
-    it("Checks if getValueFromStorage function gets a value correctly", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            Countly._internals.setValueInStorage(key, valueToStore, false);
-            expect(valueToStore).to.equal(Countly._internals.getValueFromStorage(key, false));
-            expect(null).to.equal(Countly._internals.getValueFromStorage(key));
-        });
-    });
-    it("Checks if getValueFromStorage function can not get a value if it does not exist", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            Countly._internals.setValueInStorage(key, valueToStore);
-            expect(valueToStore).to.equal(Countly._internals.getValueFromStorage(key));
-            expect(null).to.equal(Countly._internals.getValueFromStorage(key, false));
-        });
-    });
-    it("Checks if removeValueFromStorage function removes a value correctly", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            Countly._internals.setValueInStorage(key, valueToStore, false);
-            expect(valueToStore).to.equal(Countly._internals.getValueFromStorage(key, false));
-            Countly._internals.removeValueFromStorage(key, false);
-            expect(null).to.equal(Countly._internals.getValueFromStorage(key, false));
-        });
-    });
-});
 
-// check for local storage functionality with rawKey but no cookies.
-describe("Storage tests, local storage w/raw key", () => {
-    it("Checks if setValueInStorage function stores a value correctly", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            Countly._internals.setValueInStorage(key, valueToStore, undefined, true);
-            cy.getLocalStorage(`${key}`).then((value) => {
-                expect(value).to.equal(valueToStore);
+        // check basic functionality for cookies. No rawKey or useLocalstorage.
+        describe("uselocalstorage: false ", () => {
+            it("Checks if setValueInStorage function stores a cookie correctly", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    Countly._internals.setValueInStorage(key, valueToStore, false);
+                    cy.getLocalStorage(`${hp.appKey}/${key}`).then((value) => {
+                        expect(value).to.equal(null);
+                    });
+                    if (isNone || isLocal) {
+                        expect(document.cookie).to.equal("");
+                    }
+                    else {
+                        expect(document.cookie).to.include(`${hp.appKey}/${key}=${valueToStore}`);
+                    }
+                });
             });
-            cy.getLocalStorage(`${hp.appKey}/${key}`).then((value) => {
-                expect(value).to.equal(null);
+            it("Checks if getValueFromStorage function gets a value correctly", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    Countly._internals.setValueInStorage(key, valueToStore, false);
+                    if (isCookie) {
+                        expect(isNone || isLocal ? undefined : null).to.equal(Countly._internals.getValueFromStorage(key, !!isCookie));
+                    }
+                    expect(isNone || isLocal ? undefined : valueToStore).to.equal(Countly._internals.getValueFromStorage(key, false));
+                });
+            });
+            it("Checks if getValueFromStorage function can not get a value if it does not exist", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    Countly._internals.setValueInStorage(key, valueToStore);
+                    expect(isNone || isLocal ? undefined : null).to.equal(Countly._internals.getValueFromStorage(key, !!isCookie));
+                });
+            });
+            it("Checks if removeValueFromStorage function removes a value correctly", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    Countly._internals.setValueInStorage(key, valueToStore, false);
+                    expect(isNone || isLocal ? undefined : valueToStore).to.equal(Countly._internals.getValueFromStorage(key, false));
+                    Countly._internals.removeValueFromStorage(key, false);
+                    expect(isNone || isLocal ? undefined : null).to.equal(Countly._internals.getValueFromStorage(key, false));
+                });
             });
         });
-    });
-    it("Checks if getValueFromStorage function gets a value correctly", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            Countly._internals.setValueInStorage(key, valueToStore, undefined, true);
-            expect(valueToStore).to.equal(Countly._internals.getValueFromStorage(key, undefined, true));
-        });
-    });
-    it("Checks if getValueFromStorage function can not get a value if it does not exist", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            expect(null).to.equal(Countly._internals.getValueFromStorage(key, undefined, true));
-        });
-    });
-    it("Checks if removeValueFromStorage function removes a value correctly", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            Countly._internals.setValueInStorage(key, valueToStore, undefined, true);
-            expect(valueToStore).to.equal(Countly._internals.getValueFromStorage(key, undefined, true));
-            Countly._internals.removeValueFromStorage(key, undefined, true);
-            expect(null).to.equal(Countly._internals.getValueFromStorage(key, undefined, true));
-        });
-    });
-});
 
-// check for cookies functionality with rawKey but no localstorage.
-describe("Storage tests, cookies w/raw key", () => {
-    it("Checks if setValueInStorage function stores a value correctly", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            Countly._internals.setValueInStorage(key, valueToStore, false, true);
-            cy.getLocalStorage(`${key}`).then((value) => {
-                expect(value).to.equal(null);
+        // check for local storage functionality with rawKey but no cookies.
+        describe("useRawKey: true", () => {
+            it("Checks if setValueInStorage function stores a value correctly", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    Countly._internals.setValueInStorage(key, valueToStore, undefined, true);
+                    cy.getLocalStorage(`${key}`).then((value) => {
+                        if (isCookie) {
+                            expect(value).to.equal(null);
+                            expect(document.cookie).to.contain(`${key}=${valueToStore}`);
+                        }
+                        else if (isNone) {
+                            expect(value).to.equal(null);
+                        }
+                        else {
+                            expect(value).to.equal(valueToStore);
+                        }
+                    });
+                    cy.getLocalStorage(`${hp.appKey}/${key}`).then((value) => {
+                        expect(value).to.equal(null);
+                    });
+                });
             });
-            cy.getLocalStorage(`${hp.appKey}/${key}`).then((value) => {
-                expect(value).to.equal(null);
+            it("Checks if getValueFromStorage function gets a value correctly", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    Countly._internals.setValueInStorage(key, valueToStore, undefined, true);
+                    expect(isNone ? undefined : valueToStore).to.equal(Countly._internals.getValueFromStorage(key, undefined, true));
+                });
             });
-            expect(document.cookie).to.equal(`${key}=${valueToStore}`);
+            it("Checks if getValueFromStorage function can not get a value if it does not exist", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    expect(isNone ? undefined : null).to.equal(Countly._internals.getValueFromStorage(key, undefined, true));
+                });
+            });
+            it("Checks if removeValueFromStorage function removes a value correctly", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    Countly._internals.setValueInStorage(key, valueToStore, undefined, true);
+                    expect(isNone ? undefined : valueToStore).to.equal(Countly._internals.getValueFromStorage(key, undefined, true));
+                    Countly._internals.removeValueFromStorage(key, undefined, true);
+                    expect(isNone ? undefined : null).to.equal(Countly._internals.getValueFromStorage(key, undefined, true));
+                });
+            });
+        });
+
+        // check for cookies functionality with rawKey but no uselocalstorage.
+        describe("uselocalstorage: false, useRawKey: true", () => {
+            it("Checks if setValueInStorage function stores a value correctly", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    Countly._internals.setValueInStorage(key, valueToStore, false, true);
+                    cy.getLocalStorage(`${key}`).then((value) => {
+                        expect(value).to.equal(null);
+                    });
+                    cy.getLocalStorage(`${hp.appKey}/${key}`).then((value) => {
+                        expect(value).to.equal(null);
+                    });
+                    if (isNone || isLocal) {
+                        expect(document.cookie).to.include("");
+                    }
+                    else {
+                        expect(document.cookie).to.include(`${key}=${valueToStore}`);
+                    }
+                });
+            });
+            it("Checks if getValueFromStorage function gets a value correctly", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    Countly._internals.setValueInStorage(key, valueToStore, false, true);
+                    expect(isNone || isLocal ? undefined : valueToStore).to.equal(Countly._internals.getValueFromStorage(key, false, true));
+                    expect(isNone || isLocal ? undefined : null).to.equal(Countly._internals.getValueFromStorage(key, false));
+                });
+            });
+            it("Checks if getValueFromStorage function can not get a value if it does not exist", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    expect(isNone || isLocal ? undefined : null).to.equal(Countly._internals.getValueFromStorage(key, false, true));
+                });
+            });
+            it("Checks if removeValueFromStorage function removes a value correctly", () => {
+                hp.haltAndClearStorage(() => {
+                    initMain(flag);
+                    Countly._internals.setValueInStorage(key, valueToStore, false, true);
+                    expect(isNone || isLocal ? undefined : valueToStore).to.equal(Countly._internals.getValueFromStorage(key, false, true));
+                    Countly._internals.removeValueFromStorage(key, false, true);
+                    expect(isNone || isLocal ? undefined : null).to.equal(Countly._internals.getValueFromStorage(key, false, true));
+                });
+            });
         });
     });
-    it("Checks if getValueFromStorage function gets a value correctly", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            Countly._internals.setValueInStorage(key, valueToStore, false, true);
-            expect(valueToStore).to.equal(Countly._internals.getValueFromStorage(key, false, true));
-            expect(null).to.equal(Countly._internals.getValueFromStorage(key, false));
-        });
-    });
-    it("Checks if getValueFromStorage function can not get a value if it does not exist", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            expect(null).to.equal(Countly._internals.getValueFromStorage(key, false, true));
-        });
-    });
-    it("Checks if removeValueFromStorage function removes a value correctly", () => {
-        hp.haltAndClearStorage(() => {
-            initMain();
-            Countly._internals.setValueInStorage(key, valueToStore, false, true);
-            expect(valueToStore).to.equal(Countly._internals.getValueFromStorage(key, false, true));
-            Countly._internals.removeValueFromStorage(key, false, true);
-            expect(null).to.equal(Countly._internals.getValueFromStorage(key, false, true));
-        });
-    });
-});
+}
