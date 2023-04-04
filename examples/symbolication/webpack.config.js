a29597427138c3cd610d21232bc3fffe7ce6ace2
turtledreams <62231246+turtledreams@users.noreply.github.com>
2023-04-04 12:18:12 +0300

    symb example (#408)

--- examples/symbolication/webpack.config.js
+++ examples/symbolication/webpack.config.js
@@ -1,27 +1,19 @@
 const path = require('path');
-const webpack = require('webpack');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  plugins: [new webpack.ProgressPlugin()],
-  devtool: "hidden-source-map",
-
+  mode: 'none',
+  devtool: 'source-map',
   module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      include: [path.resolve(__dirname, 'src')],
-      loader: 'babel-loader'
-    }]
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
+      },
+    ],
   },
-
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()]
-  },
-
   output: {
     devtoolModuleFilenameTemplate: '[resource-path]',
-    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]'
-  }
+    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]',
+  },
 };
