c9c4ffe017b71a7aec9b99256c74521d90d927c5
turtledreams <deniztaygunerten@gmail.com>
2023-04-19 16:18:47 +0900

    boomr

--- plugin/boomerang/countly_boomerang.js
+++ plugin/boomerang/countly_boomerang.js
@@ -6,12 +6,16 @@ Countly APM based on Boomerang JS
 Plugin being used - RT, AutoXHR, Continuity, NavigationTiming, ResourceTiming
 */
 (function cly_load_track_performance() {
+    if (!window.Countly) {
+        console.error("Countly is not defined. Boomerang will not be loaded. Check the load order of Countly and Boomerang scripts.");
+        return;
+    }
     Countly = Countly || {}; // eslint-disable-line no-global-assign
     Countly.onload = Countly.onload || [];
     if (typeof Countly.CountlyClass === "undefined") {
         return Countly.onload.push(function() {
             cly_load_track_performance();
-            if (!Countly.track_performance) {
+            if (!Countly.track_performance && Countly.i) {
                 Countly.track_performance = Countly.i[Countly.app_key].track_performance;
             }
         });
