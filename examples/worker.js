e070a0869f2d8cb79ac21b235fee588a633423d4
turtledreams <deniztaygunerten@gmail.com>
2023-11-13 13:25:59 +0900

    web worker example

---
+++ examples/worker.js
@@ -0,0 +1,24 @@
+importScripts("../lib/countly.js");
+
+Countly.init({
+    app_key: "support",
+    url: "https://master.count.ly",
+    debug: true,
+    getViewUrl : function () {
+        return "https://my.url.ly";
+    }
+});
+
+onmessage = function (e) {
+    console.log('Worker: Message received from main script:' + e.data[0] + ' ' + e.data[1]);
+    const event = e.data[0]; const type = e.data[1];
+    if (type === "event") {
+        Countly.add_event(event);
+    } else if (type === "view") {
+        Countly.track_pageview(event);
+    } else if (type === "begin_session") {
+        Countly.begin_session();
+    } else if (type === "end_session") {
+        Countly.end_session(null, true);
+    }
+}
