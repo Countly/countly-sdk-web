b4f3ae38bdf5d15308abc2895b5e88e0b866a210
turtledreams <62231246+turtledreams@users.noreply.github.com>
2024-02-06 22:46:52 +0900

    types (#478)

--- examples/Angular/countly.d.ts
+++ examples/Angular/countly.d.ts
@@ -1,4 +1,25 @@
-declare module 'countly-sdk-web';
+declare module 'countly-sdk-web' {
+
+  namespace Countly {
+    /**
+     * It initializes the SDK and exposes it to the window object. It should be called before any other SDK methods in sync implementations.
+     *
+     * @param {object} config - Configuration object. Determines the SDK behavior. Some essential keys are:
+     *
+     * app_key : "app key" from your Countly server application. (MANDATORY!)
+     *
+     * url : Your Countly server URL. You may also use your own server URL or IP here. (MANDATORY!)
+     *
+     * debug : Enables SDK logs to be printed into the browser console. (default: false)
+     *
+     * For the full list of configuration options, see https://support.count.ly/hc/en-us/articles/360037441932-Web-analytics-JavaScript#h_01HABTQ439HZN7Y6A6F07Y6G0K
+     */
+    function init(config: object): void;
+  }
+
+  export default Countly;
+}
+
 interface Window {
   Countly: any;
 }
