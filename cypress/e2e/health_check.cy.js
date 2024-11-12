d09a7d9ab78bd53f52ed0516ec889244caf5af37
turtledreams <62231246+turtledreams@users.noreply.github.com>
2024-11-13 03:17:54 +0900

    SDK update and fixes

--- cypress/e2e/health_check.cy.js
+++ cypress/e2e/health_check.cy.js
@@ -10,27 +10,41 @@ function initMain() {
     });
 }
 
-describe("Health Check tests ", () => {
+describe("Health Check tests", () => {
     it("Check if health check is sent at the beginning", () => {
         hp.haltAndClearStorage(() => {
             initMain();
-            cy.intercept("https://your.domain.count.ly/i?*").as("getXhr");
-            cy.wait("@getXhr").then((xhr) => {
-                const url = new URL(xhr.request.url);
+            cy.intercept("POST", "https://your.domain.count.ly/i").as("postXhr");
+            cy.wait("@postXhr").then((xhr) => {
+                const body = xhr.request.body;
+                const params = new URLSearchParams(body);
 
-                // Test the 'hc' parameter
-                const hcParam = url.searchParams.get("hc");
-                const hcParamObj = JSON.parse(hcParam);
+                const hcParam = params.get("hc");
+                const hcParamObj = JSON.parse(decodeURIComponent(hcParam));
                 expect(hcParamObj).to.eql({ el: 0, wl: 0, sc: -1, em: "" });
 
-                // Test the 'metrics' parameter
-                const metricsParam = url.searchParams.get("metrics");
+                const metricsParam = params.get("metrics");
                 expect(metricsParam).to.equal("{\"_app_version\":\"0.0\",\"_ua\":\"abcd\"}");
 
-                // check nothing in the request queue
                 cy.fetch_local_request_queue().then((rq) => {
                     expect(rq.length).to.equal(0);
                 });
+            });
+        });
+    });
+    it("Check no health check is sent in offline mode", () => {
+        hp.haltAndClearStorage(() => {
+            Countly.init({
+                app_key: "YOUR_APP_KEY",
+                url: "https://your.domain.count.ly",
+                test_mode: true,
+                offline_mode: true
+            });
+
+            cy.intercept("POST", "https://your.domain.count.ly/i").as("postXhr");
+            cy.get('@postXhr').should('not.exist');
+            cy.fetch_local_request_queue().then((rq) => {
+                expect(rq.length).to.equal(0);
             });
         });
     });
